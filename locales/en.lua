f70c7c24648553f1226ab2f2d7c22b01b04e70b4
GaluzaCZ <galdia8@gmail.com>
2022-01-20 18:28:34 +0100

    Created en

---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+    ['press_menu'] = 'Press ~INPUT_CONTEXT~ for open ~y~menu~s~',
+    ['press_despawn'] = 'Press ~INPUT_CONTEXT~ for store this ~g~vehicle~s~',
+    ['no_cars'] = 'Empty!',
+    ['spawnpoint_not_clear'] = 'There are some vehicles!',
+    ['not_owned'] = 'This vehicle is not yours!',
+    ['impound_blip_name'] = 'Impound',
+    ['garage_blip_name'] = 'Garage',
+	['no_money'] = 'You dont have enough money! Impound cost $%s',
+}
